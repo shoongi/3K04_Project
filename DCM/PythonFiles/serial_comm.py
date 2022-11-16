e9c77785ae2c6cfe86e9aa6e27cfb6ded3957355
Ivan Lange <72510282+langei@users.noreply.github.com>
2022-11-16 18:50:55 -0500

    Add serial pack function to serial_comm.py

--- DCM/PythonFiles/serial_comm.py
+++ DCM/PythonFiles/serial_comm.py
@@ -9,23 +9,58 @@ DATASIZE    = 8
 TIMEOUT     = 1
 
 
-#Initialize serial port with defined parameters
-ser = serial.Serial(port        = SERIALPORT, 
-                    baudrate    = BAUDRATE, 
-                    bytesize    = DATASIZE, 
-                    timeout     = TIMEOUT, 
-                    stopbits    = serial.STOPBITS_ONE)
+def initializeSerial():
+#   Initialize serial port with defined parameters
+    ser = serial.Serial(port        = SERIALPORT, 
+                        baudrate    = BAUDRATE, 
+                        bytesize    = DATASIZE, 
+                        timeout     = TIMEOUT, 
+                        stopbits    = serial.STOPBITS_ONE)
+    return ser
 
-serialString = 0;
+'''
+note:   The packing function will pack the required parameters into a 40 bit integer
+        according to their required space as outlined below.
 
-while(1):
+Parameter		        Data Type	Bytes
 
-    print("printing..")
+LRL			            uint8_t		1
+URL			            uint8_t		1
+Max Sensor Rate		    uint8_t		1
+VAMP			        single		4
+VPW			            single		4
+VSensitivty		        single		4
+VRP			            uint16_t	2
+AAMP			        single		4
+APW			            single		4
+ASensitivty		        single		4
+ARP			            uint16_t	2
+PVARP			        uint16_t	2
+Activity Threshold	    uint8_t		1
+Reaction Time		    uint8_t		1
+Response Factor		    uint8_t		1
+Recovery Time		    single		4
+
+'''
+def packTransmitData(lrl, url, max_rate, vamp, vpw, vsensitivity, vrp, aamp, apw, 
+                     asensitivity, arp, pvarp, act_threshold, react_time, resp_factor, recover_time):
     
-    # Read data out of the buffer until a carriage return / new line is found
-    serialString = ser.read(5)
 
-    # Print the contents of the serial data
-    print(serialString.hex())
+    
 
-    time.sleep(5)
+def main():
+
+    ser = initializeSerial()
+    serialString = 0;
+
+    while(1):
+
+        print("printing..")
+    
+        # Read data out of the buffer until a carriage return / new line is found
+        serialString = ser.read(5)
+
+        # Print the contents of the serial data
+        print(serialString.hex())
+
+        time.sleep(5)
